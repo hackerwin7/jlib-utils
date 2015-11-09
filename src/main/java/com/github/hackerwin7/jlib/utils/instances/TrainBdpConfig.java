3f41e66874bb37f4992a853bdc9e31c25628110c
hackerwin7 <1186897618@qq.com>
2015-11-09 17:23:42 +0800

    extend the bdp config extracter

--- src/main/java/com/github/hackerwin7/jlib/utils/instances/TrainBdpConfig.java
+++ src/main/java/com/github/hackerwin7/jlib/utils/instances/TrainBdpConfig.java
@@ -1,7 +1,9 @@
 package com.github.hackerwin7.jlib.utils.instances;
 
 import com.github.hackerwin7.jlib.utils.drivers.url.URLClient;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
@@ -9,7 +11,9 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * get source host in train.bdp.com......
@@ -29,7 +33,65 @@ public class TrainBdpConfig {
      * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Throwable {
+        TrainBdpConfig tbc = new TrainBdpConfig();
+        tbc.startGetTopic();
+    }
+
+    private void startGetTopic() throws Throwable {
+        InputStream is = TrainBdpConfig.class.getClassLoader().getResourceAsStream(FILE_NAME);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        List<String> jobs = new ArrayList<>();
+        while ((line = br.readLine()) != null) {
+            if(!StringUtils.isBlank(line)) {
+                jobs.add(line);
+            }
+        }
+        br.close();
+        is.close();
+
+        for(String job : jobs) {
+            String pjobId = toParserJobId(job);
+            String jdata = JDataGetTopic(job, pjobId);
+            logger.info(job + " ---------> " + jdata);
+            Thread.sleep(500);
+        }
+    }
+
+    private static String toParserJobId(String jobId) throws Throwable {
+        if(jobId.startsWith("2002")) { //old tracker name
+            return "21" + jobId.substring(2);
+        } else {// new tracker name
+            long num = Long.valueOf(jobId);
+            return String.valueOf(num + 1);
+        }
+    }
+
+    private String JDataGetTopic(String tid, String pid) throws Throwable {
+        JSONObject jtdata = JSONObject.fromObject(URLClient.getFromUrl(BDP_URL + tid)).getJSONObject("data");
+        String ip = null;
+        if(jtdata.containsKey("source_host")) {
+            ip = jtdata.getString("source_host");
+        } else {
+            ip = "NOT FOUND key = source_host";
+        }
+        JSONObject jpdata = JSONObject.fromObject(URLClient.getFromUrl(BDP_URL + pid)).getJSONObject("data");
+        String topicStr = null;
+        if(jpdata.containsKey("db_tab_meta")) {
+            JSONArray jparr = jpdata.getJSONArray("db_tab_meta");
+            Set<String> topics = new HashSet<String>();
+            for(int i = 0; i <= jparr.size() - 1; i++) {
+                topics.add(jparr.getJSONObject(i).getString("topic"));
+            }
+            topicStr = StringUtils.join(topics, ",");
+        } else {
+            topicStr = "NOT FOUND key = db_tab_meta";
+        }
+        return ip + " =====> " + topicStr;
+    }
+
+    private void startGetIp() throws Throwable {
         InputStream is = TrainBdpConfig.class.getClassLoader().getResourceAsStream(FILE_NAME);
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line = null;
@@ -50,7 +112,7 @@ public class TrainBdpConfig {
         }
     }
 
-    private static String JDataGet(String jsonStr, String key) {
+    private String JDataGet(String jsonStr, String key) {
         JSONObject jroot = JSONObject.fromObject(jsonStr);
         JSONObject jdata = jroot.getJSONObject("data");
         if(jdata.containsKey(key)) {
