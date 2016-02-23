d1a2f626cf579b644b6b1bde6cf155d824f46ba9
hackerwin7 <1186897618@qq.com>
2016-02-23 18:07:42 +0800

    developed zk utils executors

--- src/main/java/com/github/hackerwin7/jlib/utils/executors/ZkUtilCommand.java
+++ src/main/java/com/github/hackerwin7/jlib/utils/executors/ZkUtilCommand.java
@@ -9,25 +9,26 @@ import com.github.hackerwin7.jlib.utils.drivers.zk.ZkClient;
  * Time: 1:44 PM
  * Desc:
  */
-public class ZkExecutorDemo {
+public class ZkUtilCommand {
 
     private String zkconn = "172.19.176.19:2181,172.19.176.20:2181,172.19.176.21:2181,172.19.176.22:2181,172.19.176.23:2181";
 
     public static void main(String[] args) throws Exception {
-        ZkExecutorDemo demo = new ZkExecutorDemo();
-        String cmd = args[0];
-        String path = args[1];
-        String data = args[2];
-        demo.run(cmd, path, data);
+        ZkUtilCommand demo = new ZkUtilCommand();
+        String connStr = args[0];
+        String cmd = args[1];
+        String path = args[2];
+        String data = args[3];
+        demo.run(connStr, cmd, path, data);
     }
 
-    public void run(String cmd, String path, String data) throws Exception {
-        ZkClient zk = new ZkClient(zkconn);
+    public void run(String connStr, String cmd, String path, String data) throws Exception {
+        ZkClient zk = new ZkClient(connStr);
         switch (cmd) {
             case "set":
                 zk.set(path, data);
                 break;
-            case "getOrigin":
+            case "get":
                 System.out.println(zk.get(path));
                 break;
             case "rmr":
